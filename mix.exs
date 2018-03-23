79dd41592f78996fa840b56f72ff0b9de2ed3188
Taylor Mock <tmock12@gmail.com>
2018-03-23 15:05:19 +0200

    Add ex_doc dev dependency

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule WordpressEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:httpoison, "~> 1.0"},
       {:jason, "~> 1.0"}
     ]
