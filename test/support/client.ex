196812ee38f1d59692e44e0c14304a9f131c2ef7
Taylor Mock <tmock12@gmail.com>
2018-04-08 21:39:17 +0300

    Restructure test responses

--- test/support/client.ex
+++ test/support/client.ex
@@ -1,8 +1,13 @@
 defmodule WordpressEx.Test.Client do
   @base_url Application.get_env(:wordpress_ex, :base_url)
 
-  def request(:get, @base_url <> "/" <> path, _body, _headers, _options) do
-    with {:ok, body} <- File.read("test/fixtures/#{path}_response.json"),
-         do: {:ok, %{body: body, status_code: 200}}
+  def request(method, @base_url <> "/" <> path, _body, _headers, _options) do
+    filename =
+      "test/fixtures/responses/#{method}_#{path |> String.replace("/", "_")}_response.json"
+
+    case File.read(filename) do
+      {:ok, body} -> {:ok, %{body: body, status_code: 200}}
+      {:error, _} -> IO.warn("Response file not found:" <> filename)
+    end
   end
 end
